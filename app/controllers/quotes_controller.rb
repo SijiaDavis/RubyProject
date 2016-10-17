13266659dcd817abebb5bba378ccd6f5832d11ee
Sijia Davis <arwen@ksu.edu>
2016-10-17 18:52:22 +0000

    Add screenshots

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
-
+  
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
@@ -10,12 +10,12 @@ class QuotesController < ApplicationController
     end
     redirect_to root_path
   end
-
+  
   def about
   end
-
+  
   private
-
+  
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end
